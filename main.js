1c1d7195955e6439eb3c3d47144cafb058c85ed1
Alex Crooks <alexcrooks@gmail.com>
2018-02-05 22:36:18 +0000

    Start work on auto updates

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 import { app, ipcMain } from 'electron'
+import { autoUpdater } from 'electron-updater'
 import log from 'electron-log'
 import menubar from 'menubar'
 import path from 'path'
 import JiraWorklogs from './jira-worklogs'
 
 log.transports.console.level = 'warn'
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info'
+
+log.info(app.getName());
+log.info(app.getVersion());
+
+console.log(app.getName(), app.getVersion())
 
 require('fix-path')(); // resolve user $PATH env variable
 
@@ -28,6 +36,7 @@ const installExtensions = async () => {
 console.log('userData', app.getPath('userData'));
 
 let fetchingWorklogs = false
+let willQuitApp = false
 
 // menubar
 const mb = menubar({
@@ -48,6 +57,9 @@ mb.on('ready', async () => {
   mb.tray.setTitle(' Login')
 
   console.log('app is ready'); // eslint-disable-line
+
+  if (process.env.NODE_ENV !== 'development')
+    autoUpdater.checkForUpdates()
 });
 
 mb.on('show', () => {
@@ -57,6 +69,8 @@ mb.on('show', () => {
 mb.on('hide', () => {
   mb.tray.setImage(path.join(app.getAppPath(), '/static/tray.png'))
 });
+
+app.on('before-quit', () => willQuitApp = true)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
@@ -71,6 +85,14 @@ app.on('window-all-closed', () => {
 ipcMain.on('quit', () => {
   app.quit();
 });
+
+ipcMain.on('updateInstall', (event, message) => {
+
+  console.log('Installing update')
+
+  willQuitApp = true
+  autoUpdater.quitAndInstall()
+})
 
 ipcMain.on('updateTitle', (event, title) => {
   mb.tray.setTitle(` ${title}`)
@@ -105,3 +127,26 @@ ipcMain.on('fetchWorklogs', (event, args) => {
       event.sender.send('worklogs', JSON.stringify([]))
     })
 });
+
+autoUpdater.on('checking-for-update', () => {
+  mb.webContents.send('updateChecking')
+})
+
+autoUpdater.on('update-not-available', (ev, info) => {
+
+  mb.webContents.send('updateNotAvailable')
+})
+
+autoUpdater.on('update-available', (updateInfo) => {
+
+  mb.webContents.send('updateDownloading', updateInfo)
+})
+
+autoUpdater.on('update-downloaded', (ev, info) => {
+
+  mb.webContents.send('updateReady')
+})
+
+autoUpdater.on('error', (ev, err) => {
+  mb.webContents.send('updateError')
+})
